fd1b948c59af063442177a5ee6f49e0b07cf6056
Rodrigo Dantas <drigowpd@gmail.com>
2022-09-21 00:33:03 -0300

    mais alguns ajustes

--- js/script.js
+++ js/script.js
@@ -1,126 +1,152 @@
-const table = document.querySelector(".users-table");
-const form = document.querySelector(".form-new-user");
-const deleteButton = document.querySelector(".delete-button");
+(() => {
+  const table = document.querySelector(".users-table");
+  const form = document.querySelector(".form-new-user");
 
-const getUsers = async () => {
-  const dataAPI = await fetch(`https://gorest.co.in/public/v2/users`);
-  const data = await dataAPI.json();
-  return data;
-};
-
-async function showUsers() {
-  const usuarios = await getUsers();
-
-  usuarios.forEach((element) => {
-    const tr = createTr(element);
-    table.appendChild(tr);
-  });
-}
-
-function createTr(user) {
-  const tr = document.createElement("tr");
-  const tdId = document.createElement("td");
-  const tdName = document.createElement("td");
-  const tdEmail = document.createElement("td");
-  const tdButtons = document.createElement("td");
-  tr.setAttribute("id", user.id);
-  tr.appendChild(tdId);
-  tr.appendChild(tdName);
-  tr.appendChild(tdEmail);
-  tr.appendChild(tdButtons);
-  tdId.innerHTML = user.id;
-  tdName.innerHTML = user.name;
-  tdEmail.innerHTML = user.email;
-  tdButtons.innerHTML = `<input type="submit" onclick="editUser(${user.id})" value="Editar" src="https://gorest.co.in/public/v2/users/${user.id}"  />`;
-  tdButtons.innerHTML += `<input type="submit" onclick="deleteUser(${user.id})" class="delete-button" value="Excluir" src="https://gorest.co.in/public/v2/users/${user.id}"  />`;
-
-  return tr;
-}
-
-form.addEventListener("click", (e) => {
-  e.preventDefault();
-  const url = "https://gorest.co.in/public/v2/users";
-  const nome = window.prompt("Name");
-  const email = window.prompt("Email");
-  const gender = window.prompt("Genero");
-
-  body = {
-    name: nome,
-    email: email,
-    gender: gender,
-    status: "active",
+  const getUsers = async () => {
+    try {
+      const dataAPI = await fetch(`https://gorest.co.in/public/v2/users`);
+      const data = await dataAPI.json();
+      return data;
+    } catch (err) {
+      alert("Erro ao tentar pegar os dados");
+    }
   };
 
-  createUser(url, body);
-});
+  const showUsers = async () => {
+    try {
+      const users = await getUsers();
+      users.forEach((user) => {
+        const tableRow = createTableRow(user);
+        table.appendChild(tableRow);
+      });
+    } catch (err) {
+      alert("Erro ao tentar exibir os usuários");
+    }
+  };
 
-async function createUser(url, user) {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
-    },
-    body: JSON.stringify(user),
+  const createButton = (text, callback) => {
+    const button = document.createElement("button");
+    button.innerText = text;
+    button.addEventListener("click", callback);
+    return button;
+  };
+
+  const createTableRow = (user) => {
+    const tableRow = document.createElement("tr");
+    const tableDataId = document.createElement("td");
+    const tableDataName = document.createElement("td");
+    const tableDataEmail = document.createElement("td");
+    const tableDataButtons = document.createElement("td");
+    const deleteButton = createButton("Excluir", () => deleteUser(user.id));
+    const editButton = createButton("Editar", () => editUser(user.id));
+
+    tableRow.setAttribute("id", user.id);
+    tableRow.appendChild(tableDataId);
+    tableRow.appendChild(tableDataName);
+    tableRow.appendChild(tableDataEmail);
+    tableRow.appendChild(tableDataButtons);
+    tableDataId.innerHTML = user.id;
+    tableDataName.innerHTML = user.name;
+    tableDataEmail.innerHTML = user.email;
+    tableDataButtons.appendChild(editButton);
+    tableDataButtons.appendChild(deleteButton);
+
+    return tableRow;
+  };
+
+  form.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = "https://gorest.co.in/public/v2/users";
+    const name = window.prompt("Name");
+    const email = window.prompt("Email");
+    const gender = window.prompt("Genero");
+
+    body = {
+      email,
+      gender,
+      name,
+      status: "active",
+    };
+
+    createUser(url, body);
   });
 
-  const data = await response.json();
-  const tr = createTr(data);
-  console.log(data, "data");
-  table.appendChild(tr);
-  return response.JSON;
-}
+  const createUser = async (url, user) => {
+    try {
+      const response = await fetch(url, {
+        body: JSON.stringify(user),
+        headers: {
+          Authorization:
+            "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
 
-async function deleteUser(id) {
-  const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
-    },
-    body: null,
-  });
+      const data = await response.json();
+      const tableRow = createTableRow(data);
+      table.appendChild(tableRow);
+    } catch {
+      alert("Erro ao enviar o novo usuário");
+    }
+  };
 
-  const data = response.json;
-  console.log(data);
-  deleteTr(id);
-}
+  const deleteUser = async (id) => {
+    try {
+      await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
+        headers: {
+          Authorization:
+            "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
+          "Content-Type": "application/json",
+        },
+        method: "DELETE",
+      });
 
-function deleteTr(id) {
-  const tr = document.getElementById(`${id}`);
-  tr.remove();
-}
+      deleteTableRow(id);
+    } catch {
+      alert("Não foi possível deletar");
+    }
+  };
 
-async function editUser(id) {
-  const name = window.prompt("Mudar nome:");
-  const email = window.prompt("Mudar email:");
-  const gender = window.prompt("Mudar gênero:");
+  const deleteTableRow = (id) => {
+    const tableRow = document.getElementById(`${id}`);
+    tableRow.remove();
+  };
 
-  const editedUser = { id: id, name: name, email: email, gender: gender };
+  const editUser = async (id) => {
+    try {
+      const name = window.prompt("Mudar nome:");
+      const email = window.prompt("Mudar email:");
+      const gender = window.prompt("Mudar gênero:");
 
-  const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
-    },
-    body: JSON.stringify(editedUser),
-  });
+      const editedUser = { email, gender, name };
 
-  changeTr(editedUser);
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
+      const response = await fetch(
+        `https://gorest.co.in/public/v2/users/${id}`,
+        {
+          body: JSON.stringify(editedUser),
+          headers: {
+            Authorization:
+              "Bearer ea59334639144498e128e9d3d24ee9f38ddd79f30c9d3d4c8f6e3e4c22ef2623",
+            "Content-Type": "application/json",
+          },
+          method: "PUT",
+        }
+      );
 
-function changeTr(user) {
-  console.log(user);
-  const tr = document.getElementById(`${user.id}`).childNodes;
-  tr[1].innerHTML = user.name;
-  tr[2].innerHTML = user.email;
-}
+      const data = await response.json();
+      editTableRow(data);
+      return data;
+    } catch {
+      alert("Erro ao editar usuário");
+    }
+  };
 
-showUsers();
+  const editTableRow = (user) => {
+    const tableRow = document.getElementById(`${user.id}`).childNodes;
+    tableRow[1].innerHTML = user.name;
+    tableRow[2].innerHTML = user.email;
+  };
+
+  showUsers();
+})();
